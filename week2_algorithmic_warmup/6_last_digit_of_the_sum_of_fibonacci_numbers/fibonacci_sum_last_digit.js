cff2341a933dfd950981ccbeb7e953c9034b14b9
Sergio Moreira <sergio.moreiraf@gmail.com>
2019-10-18 14:35:14 -0300

    Add week2_6

---
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+process.stdin.setEncoding('utf8');
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+rl.on('line', readLine);
+
+function readLine(line) {
+  const n = parseInt(line.toString());
+  main(n);
+  process.exit();
+}
+
+function main(n) {
+  // your code here
+}
+
+module.exports = readLine;
---
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.spec.js
@@ -0,0 +1,17 @@
+const mockProcess = require('jest-mock-process');
+
+mockProcess.mockProcessExit();
+const mockStdout = mockProcess.mockProcessStdout();
+const mockLog = mockProcess.mockConsoleLog();
+
+describe.skip('Last Digit of the Sum of Fibonacci Numbers', () => {
+  let func;
+  beforeEach(() => {
+    jest.resetModules();
+    func = require('./fibonacci_sum_last_digit');
+  });
+  test('(3) = 4', () => {
+    func('3');
+    expect(mockLog).toHaveBeenLastCalledWith('4');
+  });
+});
